c6d9fed64a8db06878a34b111da185fb342951c4
Kogan Zakhar <kogan@teamidea.ru>
2018-07-26 14:42:56 +0300

    More recorder_new updates.

--- recorder_new.py
+++ recorder_new.py
@@ -1,17 +1,20 @@
 import time
 import speech_recognition as sr
+import os
 
 # this is called from the background thread
 def callback(recognizer, audio):
     # received audio data, now we'll recognize it using Google Speech Recognition
     try:
         print ("  Recorded a phrase...")
+        output_folder = str(time.strftime('%d%m%Y'))
+        if os.path.exists(output_folder) == False:
+            os.mkdir(output_folder)
         filename = time.strftime('%H%M%S')
         data = audio.get_wav_data(convert_rate = 8000, convert_width = 2)
-        with open(filename + ".wav", "wb") as o:
+        with open(output_folder + "/" + filename + ".wav", "wb") as o:
             o.write(data)
-            print "  Wrote " + filename + ".wav"
-        print ("Listening...")
+            print ("  Wrote " + filename + ".wav")
     except sr.UnknownValueError:
         print("Wit could not understand audio")
     except sr.RequestError as e:
