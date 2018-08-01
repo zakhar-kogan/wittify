e2362b48a24d77665623e9a208a4016a1590cae7
Kogan Zakhar <kogan@teamidea.ru>
2018-08-01 16:26:02 +0300

    Scheduler finally working (almost) as planned. Further automation may include .exe embedding, but that's mostly it.

--- scheduler.py
+++ scheduler.py
@@ -1,15 +1,21 @@
-import schedule, subprocess, time, sys, os
+import schedule, subprocess, time, sys, os, time
 
-date_automatic = time.strftime("%d%m%Y")
+date_folder = time.strftime("%d%m%Y")
+output = date_folder + "/" + "output.txt"
 
-if len(sys.argv) > 1:
-    date_folder = sys.argv[1]
-    output = date_folder + "/" + "output.txt"
-else:
-    output = "output.txt"
-    date_folder = "/"
+if not os.path.isdir(date_folder):
+    os.mkdir(date_folder)
 
 if not os.path.isfile(output):
     f = open(output,"w+")
 
-schedule.every().day.at("23:45").do(subprocess.call(["python", "files_args.py", "-o", output, date_automatic]))
+def job (output, date):
+    print("Running the transcriber script on {0} folder.".format(date_folder))
+    subprocess.call(["python", "files_args.py", "-o", output, date])
+
+schedule.every().day.at("23:45").do(job, output, date_folder)
+
+while 1:
+    date_folder = time.strftime("%d%m%Y")
+    schedule.run_pending()
+    time.sleep(1)
